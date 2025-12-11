a694e0b9b131a7117ff77f81a5be48d2d143da58
ndtribk <ndtribk@gmail.com>
2025-12-11 15:32:11 +0700

    message_broker.cpp

--- Protocol/core/message_broker.cpp
+++ Protocol/core/message_broker.cpp
@@ -1,1 +1,61 @@
+#include "message_broker.h"
+using namespace std; 
 
+#include <iostream>
+#include <algorithm>
+
+namespace protocol :: core
+{
+    MessageBroker :: MessageBroker(){}
+    MessageBroker :: ~MessageBroker(){}
+
+    SubcriberId MessageBroker :: subcribe(const string &topic, Callback cb)
+    {
+        lock_guard <mutex> lock(mtx); 
+        SubcriberId id = next_id++; 
+        subs.push_back({
+            id, topic, cb
+        }); 
+        return id; 
+    }
+
+    void MessageBroker :: unsubcribe(SubcriberId id)
+    {
+        lock_guard<mutex> lock(mtx); 
+        subs.erase(remove_if(subs.begin(), subs.end(),[&](const SubEntry& s){ return s.id==id; } ), subs.end()); 
+    }
+
+    void MessageBroker :: publish(const string &topic, const Msg &payload, const MsgOptions &opt)
+    {
+        vector<Callback> cbs; 
+        {
+            lock_guard<mutex> lock(mtx); 
+            // simple ACL check : if topic has ACL and no subcriber rollback...
+            auto acl_it = topic_acl.find(topic); 
+            for (auto &s : subs)
+            {
+                if (s.topic != topic) continue; 
+                // if acl exists, we assume subscriber role membership.. 
+                cbs.push_back(s.cb); 
+            }
+        }
+        // Deliver outside lock.. 
+        for (auto &cb : cbs){
+            try {
+                cb(topic, payload); 
+            }
+            catch(const exception &e){
+                cerr <<"Broker call" << e.what() << endl; 
+            }
+            catch(...)
+            {
+                cout << "Broker callback unknown error\n"; 
+            }
+        }
+    }
+    void MessageBroker :: set_topic_acl(const string &topic, const vector<string> &allowed_roles)
+    {
+        lock_guard<mutex> lock(mtx); 
+        topic_acl[topic] = allowed_roles; 
+    }
+} // namespace protocol.. 
--- Protocol/core/message_broker.h
+++ Protocol/core/message_broker.h
@@ -1,1 +1,60 @@
+#pragma once 
+#include <functional>
 
+#include <string> 
+#include <unordered_map>
+
+#include <vector> 
+#include <mutex> 
+
+#include <memory> 
+using namespace std; 
+
+namespace protocol :: core
+{
+    enum class QoS {
+        AT_MOST_ONCE = 0, AT_LEAST_ONCE = 1
+    }; 
+
+    struct MsgOptions
+    {
+        QoS qos = QoS :: AT_LEAST_ONCE; 
+        int priority = 0; // higher -> deliver earlier
+    }; 
+
+    using Msg = vector<uint8_t>; 
+    using SubcriberId = uint64_t; 
+    using Callback = function<void(const string &topic, const Msg &payload)>;
+    
+    class MessageBroker 
+    {
+        public: 
+        MessageBroker(); 
+        ~MessageBroker(); 
+
+        // subcriber to a topic, returns subcriber id.. 
+        SubcriberId subcribe(const string &topic, Callback cb); 
+
+        // unsubcriber .. 
+        void unsubcribe(SubcriberId id); 
+
+        // publish a message.. 
+        void publish(const string &topic, const Msg &payload, const MsgOptions &opt = {}); 
+
+        // set topic ACL : allowed roles/ tags (simple)... 
+        void set_topic_acl(const string &topic, const vector<string> &allowed_roles);
+        
+        private: 
+        mutable mutex mtx; 
+        SubcriberId next_id; 
+        struct SubEntry{
+            SubcriberId id; 
+            string topic; 
+            Callback cb; 
+        }; 
+
+        vector<SubEntry> subs; 
+
+        unordered_map<string, vector<string>> topic_acl;
+    }; 
+} // namespace protocol.. 
