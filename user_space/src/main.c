f7672594b25b27203a350675e2ebd598e6962d70
ndtribk <ndtribk@gmail.com>
2025-12-18 22:43:17 +0700

    main.c

--- user_space/src/main.c
+++ user_space/src/main.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include "../hitl/hitl_interface.h"
-// #include "../hitl/hitl_interface.c"
 
-int main ()
-{
-    hitl_session_t session; 
-    hitl_decision_record_t decision; 
+int main() {
+    hitl_session_t session;
+    hitl_decision_record_t decision;
+    authority_token_t token;
 
-    hitl_init(); 
-    hitl_open_session(HITL_SCOPE_APPROVE, &session); 
+    hitl_init();
 
-    hitl_request_decision(
-        &session, 
-        "Rollback requested due to anomaly", 
-        &decision 
-    ); 
+    if (hitl_open_session(HITL_SCOPE_APPROVE, &session) != 0) {
+        printf("Failed to open HITL session\n");
+        return 1;
+    }
 
-    hitl_close_session(&session); 
-    return 0; 
+    int rc = hitl_request_decision(
+        &session,
+        "Rollback requested due to anomaly",
+        &decision,
+        &token
+    );
+
+    if (rc == 0) {
+        printf("Decision OK\n");
+        printf("Authority token: %s\n", token.token_id);
+    } else {
+        printf("Decision failed: %d\n", rc);
+    }
+
+    hitl_close_session(&session);
+    return 0;
 }
